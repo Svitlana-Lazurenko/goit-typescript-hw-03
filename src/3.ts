605be40cbd9c9dcf27af886c1b2f79ca94fe700c
Svitlana-Lazurenko <svitsvit222@gmail.com>
2023-11-25 17:12:11 +0200

    Resolve tasks

--- src/3.ts
+++ src/3.ts
@@ -8,6 +8,18 @@
 */
 
 // реалізація класу Wizard
+
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -34,6 +46,6 @@ const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp(); // Level up! New level is 16
 
 export {};
